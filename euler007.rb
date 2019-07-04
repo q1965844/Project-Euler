ca2dd97a2690749c46d300c00a0f59bc20e7f51b
陳敬旻／ジミ <q1965844@gms.nutc.edu.tw>
2019-07-04 11:44:34 +0900

    Problem 007 完了しました

--- euler007.rb
+++ euler007.rb
@@ -1,27 +1,20 @@
-def m_prime(n)
-    arr =[]
-    (2..n).each do|i| 
-        if n % i == 0  
-            arr << i
-            n = n / i
-            redo if n !=1
-        end
-    end
-    arr
-end
-
 def euler007
-    arr2 =[2]
-    i=2
-    loop do
-        if m_prime(i).count == 1
-            arr2 << m_prime(i)
+    n =10001
+    p_n = (2 * n * Math.log(n)).to_i       
+    sieve = [true] * p_n                 
+    count = 0
+    (2 .. p_n).each do |i|
+        if sieve[i]                  
+            count += 1                     
+            if count == n              
+                return i
+            end
+            ((2*i.. p_n).step(i)).each do |j|  
+                sieve[j] = false
+            end
         end
-        i +=1
-        break if arr2.count == 10000
     end
-    puts arr2.max
 end
 
-puts euler007
+
 

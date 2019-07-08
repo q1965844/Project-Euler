68dfe715f280d3747d321430e976a67afdae7fc8
陳敬旻／ジミ <q1965844@gms.nutc.edu.tw>
2019-07-08 13:31:03 +0900

    Problem 020 完了しました

---
+++ euler020.rb
@@ -0,0 +1,14 @@
+def n!(n)
+    sum =1
+    (1..n).each do |i|
+        sum *= i 
+    end
+    sum
+end
+
+def euler020
+    arr = n!(100).to_s.split(//).map{|i| i=i.to_i}
+    ans = arr.reduce(:+)
+    ans
+end
+puts euler020

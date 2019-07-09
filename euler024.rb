4cd550195152ecd066ca9191d358a109d099496a
陳敬旻／ジミ <q1965844@gms.nutc.edu.tw>
2019-07-09 17:59:34 +0900

    Problem 024 完了しました

---
+++ euler024.rb
@@ -0,0 +1,15 @@
+def permutation(string)
+    return [string] if string.size < 2
+    ch = string[0]
+    permutation(string[1..-1]).each_with_object([]) do |perm, result|
+      (0..perm.size).each { |i| result << perm.dup.insert(i,ch) }
+    end
+end
+
+def euler024
+    arr=[0,1,2,3,4,5,6,7,8,9]
+    n =1
+    arr = permutation("0123456789").sort
+    arr[100_0000-1]
+end
+
